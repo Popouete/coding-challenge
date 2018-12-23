eb4b25912036d49136f50f8e4883d8dc055fd5df
Antony Veilleux <antony.veilleux@edu.cegepgarneau.ca>
2018-12-22 23:31:37 -0500

    projet déposé sur github

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var controller = require('./controllers/Controller');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+controller(app);
+
+app.listen(3000);
---
+++ controllers/Controller.js
@@ -0,0 +1,53 @@
+var bodyParser= require('body-parser');
+var fs = require('fs');
+
+var urlencodeParser = bodyParser.urlencoded({ extended: false});
+var data=[];
+var found=false;
+var i=0;
+var jsonFile = fs.readFileSync("controllers/langage.json");
+const doc = JSON.parse(jsonFile);
+
+module.exports= function(app){
+
+app.get('/', function(req, res){
+    res.redirect('/ninjify');
+});
+
+app.get('/ninjify', function(req, res){
+    res.render('ninjify', {qs:req.query});
+});
+
+app.post('/ninjify',urlencodeParser , function(req, res){
+    found=false;
+    i=0;
+    //looking for the name
+    while (found==false && i<doc.length){
+        if (doc[i].name == req.body.x) {
+            found=true;
+        }else {
+            i++;
+        }
+    }
+
+    if(found){
+        //adding funny first name and last name
+        data=[];
+        data.push(req.body);
+        var array = ["Chan","Chung","Hitachi","Lee","Kong","Tao","WongTong","Wu"];
+        data.push({lName:array[Math.floor(Math.random() * array.length)]});
+        array = ["Caporal","Ninja","Maître","Rambo","Rocky","Senpai","Samouraï","Sensei","Yoda"];
+        data.push({fName:array[Math.floor(Math.random() * array.length)]});
+        data.push({bigName:data[2].fName+' '+data[0].x+ ' ' + data[1].lName});
+        res.render('ninjify-name', {data});
+    }else{
+        //return to index page
+        res.render('ninjify', {qs:req.query});
+    }
+});
+
+app.use(function(req, res, next){
+    res.status(404).render('404');
+});
+
+};
